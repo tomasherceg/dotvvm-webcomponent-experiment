eabac0057f7c55648bbbb181b3a7a203684f0b68
Tomáš Herceg <tomas.herceg@riganti.cz>
2021-05-26 22:43:59 +0200

    Default page added

--- DotvvmWebComponents/ViewModels/DefaultViewModel.cs
+++ DotvvmWebComponents/ViewModels/DefaultViewModel.cs
@@ -1,26 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using DotVVM.Framework.ViewModel;
-using DotVVM.Framework.Hosting;
 
 namespace DotvvmWebComponents.ViewModels
 {
     public class DefaultViewModel : DotvvmViewModelBase
     {
-        public string Test { get; set; } = "aaa";
-
-        public bool IsChecked1 { get; set; } = true;
-
-        public bool IsChecked2 { get; set; }
-
-        public string RadioSelection { get; set; } = "maverick";
-
-        public int SliderValue { get; set; } = 10;
-
-        public string IsDialogVisible { get; set; } = "hidden";
+        public bool DarkMode { get; set; }
     }
 }
+
